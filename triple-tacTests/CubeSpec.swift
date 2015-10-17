d431aa9c75d1d9b8ee2e3cb9294511a3d3fe793b
markedwardmurray <markedwardmurray@gmail.com>
2015-10-16 23:59:04 -0400

    add CubeSpec

---
+++ triple-tacTests/CubeSpec.swift
@@ -0,0 +1,29 @@
+//
+//  CubeSpec.swift
+//  triple-tac
+//
+//  Created by Mark Murray on 10/16/15.
+//  Copyright © 2015 Mark Edward Murray. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import triple_tac
+
+class CubeSpec: QuickSpec {
+    override func spec() {
+        describe("Cube") {
+            describe("initializer") {
+                it("should make 8 cubies when the size is 2") {
+                    
+                }
+                
+                it("should make 27 cubies when teh size is 3") {
+                    
+                }
+            }
+        }
+    }
+}
+
+
